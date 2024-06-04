6f6eda4c5faaf6f1df16b39f2c121c26e9c7c2c2
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-04 03:22:23 +0300

    Fixing task 2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Gather data from an API"""
 
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == '__main__':
     user_id = sys.argv[1]
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         f'https://jsonplaceholder.typicode.com/users/{user_id}').json()
     tasks = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
-    user_name = user['name']
+    user_name = user['username']
     if tasks.status_code == 200:
         with open(f'{sys.argv[1]}.csv', 'w') as file:
             writer = csv.writer(file, quoting=csv.QUOTE_ALL)
