834c2419a9a8c8af13e0fb056a68031694399557
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-04 03:56:18 +0300

    Fix  task 2 V3

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""Export to JSON"""
-
+"""for a given id return information about their TODO list"""
 import json
 import requests
 import sys
 
+
 if __name__ == '__main__':
 
     user_id = sys.argv[1]
-    response = requests.get(
+    res = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{user_id}')
     tasks = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
-    if response.status_code == 200:
-        data = response.json()
+        f'https://jsonplaceholder.typicode.com/todos',
+        params={"userId": user_id})
+    if res.status_code == 200:
+        data = res.json()
         tasks = tasks.json()
-        user_name = data['username']
-        with open(f'{user_id}.json', 'w', newline='') as file:
+        user_name = data.get("username")
+        with open(f'{user_id}.json', 'w', newline='') as f:
             json.dump({f"{user_id}": [
                 {
-                    "task": f"{task['title']}",
+                    "task": f"{task.get('title')}",
                     "completed": task.get("completed"),
-                    "username": f"{data['username']}"
+                    "username": user_name
                 } for task in tasks
             ]},
-                file)
+                f)
