787d37042df5b27fd35c902ea78973203f38c049
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-04 03:43:43 +0300

    Adding task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Export to JSON"""
+
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    response = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{user_id}')
+    tasks = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
+    if response.status_code == 200:
+        data = response.json()
+        tasks = tasks.json()
+        user_name = data['username']
+        with open(f'{sys.argv[1]}.json', 'w', newline='') as file:
+            json.dump({f'{user_id}': [
+                {
+                    "task": f"{task['title']}",
+                    "completed": task.get("completed"),
+                    "username": f"{data['username']}"
+                } for task in tasks
+            ]},
+                file)
