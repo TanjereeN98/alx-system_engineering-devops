114fd3d5c0a5f6009690efd68a154eda4c106f48
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-04 04:11:50 +0300

    Fixing task 2 V6

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,39 +1,27 @@
 #!/usr/bin/python3
-"""
- using this REST API, for a given employee ID,
- exports information about his/her TODO list progress to json file.
-"""
-import json
-from requests import get
-from sys import argv
+"""Export to JSON"""
 
+import json
+import requests
+import sys
 
 if __name__ == '__main__':
-    try:
-        id = int(argv[1])
-    except IndexError and ValueError:
-        print(f'usage: {argv[0]} <id>')
-        quit(1)
 
-    url = 'https://jsonplaceholder.typicode.com'
-    user = get(f'{url}/users/{id}').json()
-    todos = get(f'{url}/todos', params={'userId': id}).json()
-
-    if user == {}:
-        print(f'no user with id {id}')
-        quit(1)
-
-    with open(f'{id}.json', 'w') as file:
-        json.dump(
-            {
-                user['id']: [
-                    {
-                        'task': todo['title'],
-                        'completed': todo['completed'],
-                        'username': user['username']
-                    }
-                    for todo in todos
-                ]
-            },
-            file
-        )
+    user_id = sys.argv[1]
+    response = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{user_id}')
+    tasks = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
+    if response.status_code == 200:
+        data = response.json()
+        tasks = tasks.json()
+        user_name = data['username']
+        with open(f'{user_id}.json', 'w', newline='') as file:
+            json.dump({f"{user_id}": [
+                {
+                    "task": f"{task['title']}",
+                    "completed": task.get("completed"),
+                    "username": f"{data['username']}"
+                } for task in tasks
+            ]},
+                file)
