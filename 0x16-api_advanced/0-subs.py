d920057fbcab3c588f904b9a4b84fd6269547f14
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-19 02:56:59 +0300

    fixing task 1?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers of the given subreddit"""
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     try:
-        res = requests.get(
-            url, headers={'User-Agent': 'TanjereeN98'},
-            allow_redirects=False).json()
+        res = requests.get(url,
+                           headers={'User-Agent': 'TanjereeN98'},
+                           allow_redirects=False).json()
     except (Exception):
         return 0
     return res.get('data', {}).get('subscribers', 0)
