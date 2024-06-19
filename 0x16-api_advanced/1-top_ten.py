9c5bf81fadecf09a36de84ba4b646d5a67d04a2c
TanjereeN98 <ahmadcyc35@gmail.com>
2024-06-19 03:05:33 +0300

    Adding task 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,3 +3,17 @@
 hot posts listed for a given subreddit"""
 
 import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts"""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    res = requests.get(
+        url, headers={'User-Agent': 'TanjereeN98'},
+        allow_redirects=False)
+    if res.status_code == 200:
+        posts = res.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post.get('data', {}).get('title', ''))
+    else:
+        print(None)
